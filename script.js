d5559172effdaf3be5a219fff1a9e0270d4745c2
elzeveta <eliza.amanbaeva@gmail.com>
2021-06-04 13:55:32 -0700

    Added functionality

---
+++ script.js
@@ -0,0 +1,20 @@
+function calculate(){
+    var bill = document.getElementById('bill').value;
+    var service = document.getElementById('service').value;
+    var peopleCount = document.getElementById('peopleCount').value;
+
+    if (bill === '' || service == 0) {
+        alert('Please enter values');
+        return;
+    }
+
+    if (peopleCount === '' || peopleCount <= 1) {
+        peopleCount = 1;
+        document.getElementById('each').style.display = 'none';
+    } else {
+        document.getElementById('each').style.display = 'block';
+    }
+
+    var total = (bill * service) / peopleCount;
+    total = Math.round(total * 100) / 100;
+    total = total.toFixed(2);
